5201ae608b81bc25fc93e428849401579d37a367
raahguu <raahguu@outlook.com>
2026-01-12 16:53:32 +0800

    Challenges are now dynamic

--- challenges.py
+++ challenges.py
@@ -1,7 +1,11 @@
+import math
+
+from CTFd.models import Solves, db
 from CTFd.plugins.challenges import BaseChallenge
-from CTFd.models import db
 from CTFd.utils.user import get_current_user
+
 from .models import DockerChallenge
+
 
 class DockerChallengeType(BaseChallenge):
     id = "docker"
@@ -11,13 +15,13 @@ class DockerChallengeType(BaseChallenge):
     templates = {
         "create": "/plugins/docker_challenges/templates/create.html",
         "update": "/plugins/docker_challenges/templates/update.html",
-        "view": "/plugins/docker_challenges/templates/view.html"
+        "view": "/plugins/docker_challenges/templates/view.html",
     }
 
     scripts = {
         "create": "/plugins/docker_challenges/templates/create.js",
         "update": "/plugins/docker_challenges/templates/update.js",
-        "view": "/plugins/docker_challenges/templates/view.js"
+        "view": "/plugins/docker_challenges/templates/view.js",
     }
 
     route = "/plugins/docker_challenges/templates/"
@@ -25,14 +29,74 @@ class DockerChallengeType(BaseChallenge):
     challenge_model = DockerChallenge
 
     @classmethod
+    def calculate_value(cls, challenge):
+        """
+        Calculate the current value of a challenge based on solves.
+        This implements dynamic scoring logic.
+        """
+        Model = cls.challenge_model
+
+        solve_count = (
+            Solves.query.join(Model, Solves.challenge_id == Model.id)
+            .filter(
+                Solves.challenge_id == challenge.id,
+                Model.id == challenge.id,
+            )
+            .count()
+        )
+
+        # Get the dynamic parameters
+        minimum = challenge.minimum
+        initial = challenge.initial
+        decay = challenge.decay
+
+        # Calculate value based on function type
+        if challenge.function == "logarithmic":
+            value = (((minimum - initial) / (decay**2)) * (solve_count**2)) + initial
+        else:  # linear
+            value = (minimum - initial) / decay * solve_count + initial
+
+        value = math.ceil(value)
+
+        # Clamp to min
+        if value < minimum:
+            value = minimum
+
+        return value
+
+    @classmethod
+    def read(cls, challenge):
+        """
+        This method is in used to access the data of a challenge in a format processable by the front end.
+        """
+        data = super().read(challenge)
+
+        # Add dynamic scoring info
+        data["initial"] = challenge.initial
+        data["minimum"] = challenge.minimum
+        data["decay"] = challenge.decay
+        data["function"] = challenge.function
+
+        # Calculate and add current value
+        data["value"] = cls.calculate_value(challenge)
+
+        return data
+
+    @classmethod
     def create(cls, request):
         """
         This method is used to process the challenge creation request.
-
-        :param request:
-        :return:
         """
         data = request.form or request.get_json()
+
+        # Ensure dynamic scoring fields are present with defaults
+        data.setdefault("initial", 500)
+        data.setdefault("minimum", 100)
+        data.setdefault("decay", 50)
+        data.setdefault("function", "logarithmic")
+
+        # Remove 'value' if present since we calculate it dynamically
+        data.pop("value", None)
 
         challenge = cls.challenge_model(**data)
 
@@ -40,3 +104,24 @@ class DockerChallengeType(BaseChallenge):
         db.session.commit()
 
         return challenge
+
+    @classmethod
+    def update(cls, challenge, request):
+        """
+        This method is used to update the information associated with a challenge.
+        """
+        data = request.form or request.get_json()
+
+        for attr, value in data.items():
+            # Don't update value directly since it's calculated
+            if attr != "value":
+                setattr(challenge, attr, value)
+
+        db.session.commit()
+        return challenge
+
+    @classmethod
+    def solve(cls, user, team, challenge, request):
+        super().solve(user, team, challenge, request)
+
+        DockerChallengeType.calculate_value(challenge)
--- models.py
+++ models.py
@@ -1,10 +1,18 @@
-from CTFd.models import db, Challenges
 from CTFd.exceptions.challenges import ChallengeCreateException
+from CTFd.models import Challenges, db
+
 
 class DockerChallenge(Challenges):
     __tablename__ = "docker_challenges"
-    id = db.Column(db.Integer, db.ForeignKey("challenges.id", ondelete="CASCADE"), primary_key=True)
+    id = db.Column(
+        db.Integer, db.ForeignKey("challenges.id", ondelete="CASCADE"), primary_key=True
+    )
     image = db.Column(db.String(128))
+
+    # initial = db.Column(db.Integer, default=0)
+    # minimum = db.Column(db.Integer, default=0)
+    # decay = db.Column(db.Integer, default=0)
+    # function = db.Column(db.String(32), default="logarithmic")
 
     __mapper_args__ = {
         "polymorphic_identity": "docker",
@@ -17,13 +25,22 @@ class DockerChallenge(Challenges):
             self.image = kwargs["image"]
         except KeyError:
             raise ChallengeCreateException("Missing image value for challenge")
-        
-    
+        try:
+            self.value = kwargs["initial"]
+        except KeyError:
+            raise ChallengeCreateException("Missing initial value for challenge")
+
+        self.minimum = kwargs.get("minimum", 100)
+        self.decay = kwargs.get("decay", 50)
+        self.function = kwargs.get("function", "logarithmic")
+
 
 class UserContainer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete="CASCADE"))
-    challenge_id = db.Column(db.Integer, db.ForeignKey("challenges.id", ondelete="CASCADE"))
+    challenge_id = db.Column(
+        db.Integer, db.ForeignKey("challenges.id", ondelete="CASCADE")
+    )
     container_id = db.Column(db.String(64))
-    ip = db.Column(db.String(64)) 
-    expiry_time = db.Column(db.Integer) # UNIX timestamp
+    ip = db.Column(db.String(64))
+    expiry_time = db.Column(db.Integer)  # UNIX timestamp
